c23e48926cdf57b427d67c597fe56dfd26063dfc
cstone24 <97609348+cstone24@users.noreply.github.com>
2023-08-03 15:46:49 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -6,6 +6,7 @@ public class Game {
 	Scanner userGuess = new Scanner(System.in);
 	public int guess;
 	final int NUMGUESS = 5;
+	final int LAST_GUESS_CHECK = NUMGUESS - 1;
 	
 	public void printStartMessage() {
 		System.out.println("I have thought of a random number between 0-100. You have 5 guesses. Good luck!");
@@ -15,9 +16,9 @@ public class Game {
 		for (int i=0; i<NUMGUESS; i++) {	
 				System.out.print("Your Guess: ");
 				guess = userGuess.nextInt();
-				if (guess < randomNumber && i!=4) {
+				if (guess < randomNumber && i!=LAST_GUESS_CHECK) {
 					System.out.println("Your number is less than the Random Number.");
-				} else if (guess > randomNumber && i!=4) {
+				} else if (guess > randomNumber && i!=LAST_GUESS_CHECK) {
 					System.out.println("Your number is greater than the Random Number.");
 				} else if (guess == randomNumber){
 					System.out.println("You guessed it! The number was " + randomNumber + ".");
